485a4e5d5512d7adc7e8b30d9111f582e3399b70
pranshu.bisht <pranshu.bisht@tc-pl.in>
2024-08-04 21:36:48 +0530

    double

--- tina/config.ts
+++ tina/config.ts
@@ -6,7 +6,7 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
 	branch,
-	clientId: null, // Get this from tina.io
+	clientId: '2f67f94f-c7b4-41cf-b923-c406ff3eadcb', // Get this from tina.io
 	token: null, // Get this from tina.io
 
 	build: {
