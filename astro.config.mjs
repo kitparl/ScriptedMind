e5a7e0f7e663361aa472cc7343bf78bed06961dd
pranshu.bisht <pranshu.bisht@tc-pl.in>
2024-08-11 22:45:44 +0530

    added config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig , squooshImageService} from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
@@ -7,6 +7,9 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pranshu.tech', // Write here your website url
+ image: {
+    service: squooshImageService(),
+  },
   markdown: {
     remarkPlugins: [remarkReadingTime],
     drafts: true,
