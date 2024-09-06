ecebdc769b23c70e4dd39a11126b6f94a3c351cd
kitparl <pranshu.java@gmail.com>
2024-09-06 22:44:26 +0530

    added config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-import { defineConfig, squooshImageService } from 'astro/config'
+import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import { remarkReadingTime } from './src/utils/readTime.ts'
 import react from '@astrojs/react';
 
 // https://astro.build/config
@@ -14,8 +13,8 @@ export default defineConfig({
       syntaxHighlight: 'shiki',
       shikiConfig: {
         experimentalThemes: {
-          light: 'vitesse-light',
-          dark: 'material-theme-palenight',
+          light: 'solarized-light', // Change this to test different themes
+          dark: 'solarized-dark',   // Change this to test different themes
         },
         wrap: true
       },
@@ -26,24 +25,11 @@ export default defineConfig({
   ],
   markdown: {
     shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://shiki.style/themes
-      // theme: "dracula",
-      // Alternatively, provide multiple themes
-      // https://shiki.style/guide/dual-themes
       themes: {
-        light: "dracula",
-        dark: "dracula",
+        light: 'solarized-light', // Change this to test different themes
+        dark: 'solarized-dark',   // Change this to test different themes
       },
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://shiki.style/languages
-      langs: [],
-      // Enable word wrap to prevent horizontal scrolling
       wrap: true,
-      // Add custom transformers: https://shiki.style/guide/transformers
-      // Find common transformers: https://shiki.style/packages/transformers
-      transformers: [],
     },
   },
   vite: {
