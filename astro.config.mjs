cbbdf3dcb17cb5b1a526c2c6a1f586e8c6526613
kitparl <pranshu.java@gmail.com>
2024-09-06 22:33:12 +0530

    added

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pranshu.tech',
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-    drafts: true,
-    shikiConfig: {
-      theme: 'material-theme-palenight',
-      wrap: true
-    }
-  },
   integrations: [
     react(),
     mdx({
@@ -32,6 +24,28 @@ export default defineConfig({
     sitemap(),
     tailwind()
   ],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      // theme: "dracula",
+      // Alternatively, provide multiple themes
+      // https://shiki.style/guide/dual-themes
+      themes: {
+        light: "dracula",
+        dark: "dracula",
+      },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
+  },
   vite: {
     optimizeDeps: {
       noDiscovery: true,
