8792ac6af5ee4b84fb8cbdd17e1933714a75d0fb
kitparl <pranshu.java@gmail.com>
2024-09-12 00:49:20 +0530

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   adapter: node({
     mode: 'standalone', // Set the mode to 'standalone'
   }),
